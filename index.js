f5fd8aa3b6346cfcedcc2e407c8c9b42940b4254
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-04-28 19:38:09 -0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+const fer = new MissionCommander("Fer")
+const rodrigo = new MissionCommander("Rodrigo")
+console.log(carlo.name)
+console.log(fer.name)
+console.log(rodrigo.name)
